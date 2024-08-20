8e8fd9d524eb652c7583fd872ebbe18988247edb
lowpolytree <minratos@gmail.com>
2024-08-20 19:41:48 -0400

    Added Decrement and Reset button.

--- src/Counter.js
+++ src/Counter.js
@@ -7,6 +7,8 @@ function Counter(){
     <div>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count+1)}>Increment</button>
+      <button disabled={count <= 0} onClick={() => setCount(count-1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
